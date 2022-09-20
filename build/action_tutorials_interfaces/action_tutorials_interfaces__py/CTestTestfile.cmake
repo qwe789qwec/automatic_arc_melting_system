56a807b44ff3f36f3e7e4b6f845be54f58d60e27
Wei-Sheng Wang <qwe789qwec@gmail.com>
2022-09-20 18:33:00 +0900

    add cpp action

---
+++ build/action_tutorials_interfaces/action_tutorials_interfaces__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arc/Documents/automatic_arc_melting_system/build/action_tutorials_interfaces/action_tutorials_interfaces__py
+# Build directory: /home/arc/Documents/automatic_arc_melting_system/build/action_tutorials_interfaces/action_tutorials_interfaces__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
