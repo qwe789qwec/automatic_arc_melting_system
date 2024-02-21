177c879b3857deb742b93ded2615f7f13077694d
qwe789qwec <qwe789qwec@gmail.com>
2024-02-21 19:12:39 +0900

    test slider for new process

--- src/slider_control/src/slider_unitest.cpp
+++ src/slider_control/src/slider_unitest.cpp
@@ -8,13 +8,13 @@
 #include "slider_control/slider.hpp"
 #include "tcp_handle/tcp_socket.hpp"
 #include <unistd.h>
-#include <math.h>
 
 using std::placeholders::_1;
 using namespace std::chrono_literals;
 
 std::string slider_ip = "192.168.0.3";
 int slider_port = 64511;
+slider slider(slider_ip, slider_port);
 
 int slider_client(std::string action)
 {
@@ -25,7 +25,7 @@ int slider_client(std::string action)
 	auto request = std::make_shared<msg_format::srv::ProcessService::Request>();
 	request->action = action;
 
-	while (!slider_process->wait_for_service(1s))
+	while (!slider_process->wait_for_service(3s))
 	{
 		if (!rclcpp::ok())
 		{
@@ -63,10 +63,21 @@ public:
 private:
 	void topic_callback(const msg_format::msg::ProcessMsg::SharedPtr msg) const
 	{
-		std::string message;
-		message = msg->process;
-        
-        slider_client("slider ok");
+		std::string message = msg->process;
+		static std::string step = "slider init";
+
+		RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "message: %s", message.c_str());
+		RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "step: %s", step.c_str());
+		
+		if(message.compare(step) != 0){
+			step = message;
+			RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "step message: %s", message.c_str());
+			bool action_result = slider.make_action(message);
+			if(!action_result){
+				RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "error cannot make action");
+			}
+			slider_client("slider standby");
+		}
 	}
 	rclcpp::Subscription<msg_format::msg::ProcessMsg>::SharedPtr subscription_;
 };
@@ -74,41 +85,6 @@ private:
 int main(int argc, char *argv[])
 {
 	rclcpp::init(argc, argv);
-	
-	slider slider(slider_ip, slider_port);
-	std::string center_x = slider.length2string(57*1000);
-	std::string center_xy = slider.length2string(57*1000) + slider.length2string(30*1000);
-	std::string center_yz = slider.length2string(30*1000) + slider.length2string(74.5*1000);// y 30 z 74.5
-	// std::string center_yz = "0000753000012304";// y 30 z 74.5
-	std::string zero = "0000000000000000";
-	std::string home = "0001117000013880";// y 70 and z 80
-	std::string route_1 = "0000EA60000124F8";// y 60 z 75
-	std::string route_2 = "0000C35000011D28";// y 50 z 73
-	std::string route_3 = "00009C4000011940";// y 40 z 72
-
-
-	slider.move(motor_x, center_x, "08");// x 57
-	slider.curve_move(motor_y, motor_z, home);// x 57 y 70 z 80
-	usleep(1000 * 5000);
-	slider.curve_move(motor_y, motor_z, route_1);// x 57 y 60 z 75
-	slider.curve_move(motor_y, motor_z, route_2);// x 57 y 50 z 73
-	slider.curve_move(motor_y, motor_z, route_3);// x 57 y 40 z 72
-	slider.curve_move(motor_y, motor_z, center_yz);// x 57 y 30 z 74.5
-	
-
-	int sep = 16;
-	for(int i = 0; i < sep; i++){
-		slider.curve_move(motor_x, motor_y, slider.count_circle(7000, sep, i, center_xy));
-	}
-	slider.move(motor_x, center_x, "08");// x 57
-	slider.curve_move(motor_y, motor_z, center_yz);// x 57 y 30 z 74.5
-	slider.curve_move(motor_y, motor_z, route_3);// x 57 y 40 z 72
-	slider.curve_move(motor_y, motor_z, route_2);// x 57 y 50 z 73
-	slider.curve_move(motor_y, motor_z, route_1);// x 57 y 60 z 75
-	slider.curve_move(motor_y, motor_z, home);// x 57 y 70 z 80
-	// slider.move(motor_x, "0000EA60");
-
-
 	rclcpp::spin(std::make_shared<SliderSubscriber>());
 
 	rclcpp::shutdown();
