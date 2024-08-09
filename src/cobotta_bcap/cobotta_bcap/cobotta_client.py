b7230c8f18d2a76c1259d4b54c5ec4366c1d6221
qwe789qwec <qwe789qwec@gmail.com>
2024-08-09 15:28:31 +0900

    cobotta io val test

--- src/cobotta_bcap/cobotta_bcap/cobotta_client.py
+++ src/cobotta_bcap/cobotta_bcap/cobotta_client.py
@@ -1,5 +1,5 @@
 import sys
-from tcp_format.srv import CobottaBcap
+from msg_format.srv import TestSrv
 
 import rclpy
 from rclpy.node import Node
@@ -9,14 +9,14 @@ class CobottaClientAsync(Node):
 
     def __init__(self):
         super().__init__('cobotta_client_async')
-        self.cli = self.create_client(CobottaBcap, 'cobotta_bcap')
+        self.cli = self.create_client(TestSrv, 'test_srv')
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
-        self.req = CobottaBcap.Request()
+        self.req = TestSrv.Request()
 
-    def send_request(self, action, target):
-        self.req.action = action
-        self.req.target = target
+    def send_request(self, actionOne, actionTwo):
+        self.req.action_1 = actionOne
+        self.req.action_2 = actionTwo
         self.future = self.cli.call_async(self.req)
         rclpy.spin_until_future_complete(self, self.future)
         return self.future.result()
@@ -28,8 +28,8 @@ def main(args=None):
     cobotta_client = CobottaClientAsync()
     response = cobotta_client.send_request(str(sys.argv[1]), str(sys.argv[2]))
     cobotta_client.get_logger().info(
-        'action: %s \n target: %s \n status: %s \n message: %s' %
-        (str(sys.argv[1]), str(sys.argv[2]), response.status, response.message))
+        'action: %s \n target: %s \n message: %s' %
+        (str(sys.argv[1]), str(sys.argv[2]), response.result))
 
     cobotta_client.destroy_node()
     rclpy.shutdown()
--- src/cobotta_bcap/cobotta_bcap/io_control_service.py
+++ src/cobotta_bcap/cobotta_bcap/io_control_service.py
@@ -1,23 +1,22 @@
-from tcp_format.srv import CobottaBcap
+from msg_format.srv import TestSrv
 from .pybcapclient.bcapclient import BCAPClient
 
 import rclpy
 from rclpy.node import Node
 
+host = "192.168.0.11"
+port = 5007
+timeout = 2000
+
 class CobottaActionService(Node):
 
     def __init__(self):
         super().__init__('cobotta_action')
-        self.srv = self.create_service(CobottaBcap, 'cobotta_bcap', self.cobotta_act)
+        self.srv = self.create_service(TestSrv, 'test_srv', self.cobotta_act)
 
     def cobotta_act(self, request, response):
-        self.get_logger().info('Incoming request\naction: %s target: %s' % (request.action, request.target))
-        response.status = 'init status'
-        response.message = 'init msg'
-
-        host = "192.168.0.1"
-        port = 5007
-        timeout = 2000
+        self.get_logger().info('Incoming request\nactionOne: %s actionTwo: %s' % (request.action_1, request.action_2))
+        response.result = 'init status'
 
         # Connection processing of tcp communication
         m_bcapclient = BCAPClient(host, port, timeout)
@@ -36,48 +35,29 @@ class CobottaActionService(Node):
         # Connect to RC8 (RC8(VRC)provider)
         hCtrl = m_bcapclient.controller_connect(Name, Provider, Machine, Option)
         print("Connect RC8")
-        # get IO128 Object Handl
-        IOHandl = 0
+
+        ioNum = request.action_1
         
-        ioact = request.action + request.target
-        IOHandl = m_bcapclient.controller_getvariable(hCtrl, ioact, "")
+        # get I[1] Object Handl
+        IHandl = 0
+        IHandl = m_bcapclient.controller_getvariable(hCtrl, ioNum, "")
         # read value of I[1]
-        retIO = m_bcapclient.variable_getvalue(IOHandl)
-        print("Read Variable" + ioact + " = %s" % retIO)
-
-        # read value of IO[128]
-        # switching IO state
-        newval = not(retIO)
-        # write value of IO[128]
-        m_bcapclient.variable_putvalue(IOHandl, newval)
-        print("Write Variable :newval = %s" % newval)
-        # read value of IO[128]
-        retIO = m_bcapclient.variable_getvalue(IOHandl)
-        print("Read Variable" + ioact + " = %s" % retIO)
-
-        # read and write value of IO[130]-[145]
-        # get Object Handl
-        IOWHandl = 0
-        IOWHandl = m_bcapclient.controller_getvariable(hCtrl, "IOW130", "")
-
-        # read value
-        retIOW = m_bcapclient.variable_getvalue(IOWHandl)
-        print("Read Variable IOW130 = %s" % retIOW)
-
-        # write value
-        writevalue = -1  # writevalue = 0b1111111111111111
-        m_bcapclient.variable_putvalue(IOWHandl, writevalue)
-        # read value
-        retIOW = m_bcapclient.variable_getvalue(IOWHandl)
-        print("Read Variable IOW130 = %s" % retIOW)
+        retI = m_bcapclient.variable_getvalue(IHandl)
+        print("Read Variable" + ioNum + "= %d" % retI)
+        # Generate random value
+        newval = int(request.action_2)
+        # write value of I[1]
+        m_bcapclient.variable_putvalue(IHandl, newval)
+        print("Write Variable :newval = %d" % newval)
+        # read value of I[1]
+        retI = m_bcapclient.variable_getvalue(IHandl)
+        print("Read Variable I[1] = %d" % retI)
 
         # Disconnect
-        if(IOHandl != 0):
-            m_bcapclient.variable_release(IOHandl)
-            print("Release" + ioact)
-        if(IOWHandl != 0):
-            m_bcapclient.variable_release(IOWHandl)
-            print("Release IOW130")
+        if(IHandl != 0):
+            m_bcapclient.variable_release(IHandl)
+            print("Release IHandl")
+
         # End If
         if(hCtrl != 0):
             m_bcapclient.controller_disconnect(hCtrl)
@@ -86,8 +66,7 @@ class CobottaActionService(Node):
         m_bcapclient.service_stop()
         print("B-CAP service Stop")
 
-        response.status = request.action
-        response.message = request.target
+        response.result = "Read Variable I[1] = %d" % retI
         return response
 
 
