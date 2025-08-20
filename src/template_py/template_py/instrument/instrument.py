5bf3ac1dea7b563c03abd0950b2c01cbf81b5efa
qwe789qwec <qwe789qwec@gmail.com>
2025-08-20 21:04:38 +0900

    update py template

---
+++ src/template_py/template_py/instrument/instrument.py
@@ -0,0 +1,218 @@
+import sys
+sys.path.append('./pybcapclient')
+import rclpy
+import time
+from rclpy.node import Node
+
+from ..pybcapclient.bcapclient import BCAPClient
+from ros2_utils_py.service_utils import get_command
+
+import argparse
+
+# host = "192.168.0.1"
+
+class cobotta:
+
+    ZERO = 0
+    ONE = 1
+    TWO = 2
+
+    NO_1 = "I[1]"
+    NO_2 = "I[2]"
+    NO_3 = "I[3]"
+
+    def __init__(self, host, port = 5007, timeout = 2000):
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+        # open connection
+        self.bcap = BCAPClient(self.host, self.port, self.timeout)
+
+        # start b_cap Service send SERVICE_START packet
+        self.bcap.service_start("")
+        self.valueHandle = 0
+        self.taskHandle = 0
+
+        # set Parameter
+        Name = ""
+        Provider = "CaoProv.DENSO.VRC"
+        Machine = "localhost"
+        Option = ""
+
+        # Connect to RC8 (RC8(VRC)provider)
+        self.hCtrl = self.bcap.controller_connect(Name, Provider, Machine, Option)
+
+        self.robotHandle = 0
+        self.robotHandle = self.bcap.controller_getrobot(self.hCtrl,"Arm","")
+
+        # set ExtSpeed Speed,Accel,Decel
+        Command = "ExtSpeed"
+        Speed = 75
+        Accel = 50
+        Decel = 50
+        Param = [Speed,Accel,Decel]
+        self.bcap.robot_execute(self.robotHandle,Command,Param)
+
+    def gotoPoint(self, point):
+        
+        # Take Arm
+        Command = "TakeArm"
+        Param = [0,0]
+        self.bcap.robot_execute(self.robotHandle,Command,Param)
+
+        # motor on
+        Command = "Motor"
+        Param = [1,0]
+        self.bcap.robot_execute(self.robotHandle,Command,Param)
+
+        Comp = 1
+        Pose = [point,"P","@0"]
+        self.bcap.robot_move(self.robotHandle,Comp,Pose,"")
+        # Move P,@0 P[point]
+
+        # Give Arm
+        Command = "GiveArm"
+        Param = None
+        self.bcap.robot_execute(self.robotHandle,Command,Param)
+
+    def runTask(self, task):
+        ### get task Object Handl
+        if(self.taskHandle == 0):
+            self.taskHandle = self.bcap.controller_gettask(self.hCtrl,task,"")
+
+        #Start pro1
+        #mode  1:One cycle execution, 2:Continuous execution, 3:Step forward
+        mode = 1
+        taskStatus = self.bcap.task_start(self.taskHandle, mode, "")
+        
+        status = False
+        while True:
+            taskStatus = self.bcap.task_execute(self.taskHandle,"GetStatus")
+            if(taskStatus != 3):
+                status = True
+                break
+        
+        return status
+
+    def changeValue(self, ioNum, value):
+        # get I[1] Object Handl
+        if(self.valueHandle == 0):
+            self.valueHandle = self.bcap.controller_getvariable(self.hCtrl, ioNum, "")
+        # write value
+        self.bcap.variable_putvalue(self.valueHandle, int(value))
+        # read value of I[1]
+        retI = self.bcap.variable_getvalue(self.valueHandle)
+        return retI
+    
+    def readValue(self, ioNum):
+        # get I[1] Object Handl
+        if(self.valueHandle == 0):
+            self.valueHandle = self.bcap.controller_getvariable(self.hCtrl, ioNum, "")
+        # read value
+        value = self.bcap.variable_getvalue(self.valueHandle)
+        return value
+
+    def runPath(self, path, task):
+        if not path:
+            print("Path is empty")
+            return
+        path.insert(0, self.HOME_POINT)
+        pathlength = len(path)
+        
+        for i in range(pathlength-1):
+            self.gotoPoint(path[i])
+            time.sleep(0.5)
+
+        variable = self.VAR_TAKE if "take" in task else self.VAR_PUT
+        self.changeValue(variable, path[-1])
+        self.runTask(task)
+
+        for i in reversed(range(pathlength-1)):
+            self.gotoPoint(path[i])
+            time.sleep(0.5)
+    
+    def make_action(self, step):
+        command = get_command(step, "cobotta")
+        action = "none"
+        if command == "test" or command == "init":
+            action = command
+        elif command == "none":
+            return "none"
+        elif command == "error":
+            return "error"
+
+        token = command.split("_")
+        if len(token) < 2 and action == "none":
+            return "error"
+        
+        action = token[1]
+
+        try:
+            if action == "init":
+                self.runTask(self.TASK_INIT)
+                return "standby"
+            elif action == "test":
+                self.gotoPoint(self.TEST_POINT)
+                time.sleep(0.5)
+                return "standby"
+            elif action == "takeCup":
+                task = self.TASK_TAKE_CUP
+            elif action == "putCup":
+                task = self.TASK_PUT_CUP
+            elif action == "takeDose":
+                task = self.TASK_TAKE_DOSE
+            elif action == "putDose":
+                task = self.TASK_PUT_DOSE
+            
+            position = token[2]
+            if position == "stock":
+                self.runPath([self.STOCK_P1, self.STOCK_END], task)
+            elif position == "weight":
+                if action.find("Cup") != -1:
+                    self.runPath([self.WEIGHT_P1, self.WEIGHT_CUP], task)
+                else:
+                    self.runPath([self.WEIGHT_P1, self.WEIGHT_DOSE], task)
+            elif position == "shelf3":
+                self.runPath([self.SHELF3_END], task)
+            elif position == "shelf2":
+                self.runPath([self.SHELF2_P1, self.SHELF2_END], task)
+            elif position == "arc":
+                self.runPath([self.ARC_P1, self.ARC_P2, self.ARC_STANDBY], task)
+            else:
+                return "error"
+            
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            print("Trying to reconnect to cobotta, maybe no this name")
+            return "error"  
+        
+        return "standby"
+    def __del__(self):
+
+        # motor off
+        Command = "Motor"
+        Param = [0,0]
+        self.bcap.robot_execute(self.robotHandle,Command,Param)
+
+        # Give Arm
+        Command = "GiveArm"
+        Param = None
+        self.bcap.robot_execute(self.robotHandle,Command,Param)
+
+        # Disconnect
+        if self.valueHandle != 0:
+            self.bcap.variable_release(self.valueHandle)
+
+        if self.taskHandle != 0:
+            self.bcap.variable_release(self.taskHandle)
+
+        if self.robotHandle != 0:
+            self.bcap.robot_release(self.robotHandle)
+
+        # End If
+        if self.hCtrl != 0:
+            self.bcap.controller_disconnect(self.hCtrl)
+            
+        # End If
+        self.bcap.service_stop()
+        print("B-CAP service Stop")
---
+++ src/template_py/template_py/instrument/instrument_node.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import rclpy
+import time
+import threading
+from rclpy.node import Node
+
+from msg_format.srv import ProcessService
+from msg_format.msg import ProcessMsg
+
+from ros2_utils_py.service_utils import call_service
+
+from .Instrument import Instrument
+
+class InstrumentNode(Node):
+    """
+    Instrument Node class that subscribes to a topic and processes messages.
+    """
+    def __init__(self):
+        super().__init__('Instrument_subscriber')
+        
+        # Instrument parameters
+        self.host = "192.168.0.999"
+        self.port = 7777
+        self.timeout = 2000
+        
+        # initial state
+        self.last_process = "Instrument init"
+        self.processing_lock = threading.Lock()
+        self.service_in_progress = False
+        
+        # create Instrument instance
+        self.process_client = self.create_client(ProcessService, 'process_service')
+        
+        # create subscriber
+        self.subscription = self.create_subscription(
+            ProcessMsg,
+            'topic',
+            self.listener_callback,
+            10)
+        
+        self.get_logger().info('Cobotta node initialized')
+    
+    def listener_callback(self, msg):
+        process = msg.process
+        
+        if process != self.last_process:
+            self.last_process = process
+            self.get_logger().info(f'Processing new step: {process}')
+            
+            try:
+                cobotta_handle = cobotta(self.host, self.port, self.timeout)
+                action_result = cobotta_handle.make_action(process)
+                
+                if action_result != "error":
+                    success = call_service(
+                        client=self.process_client,
+                        logger=self.get_logger(),
+                        action="cobotta_standby",
+                        service_name="process_service"
+                    )
+                    
+                    if not success:
+                        self.get_logger().error('Failed to call standby service')
+                else:
+                    self.get_logger().error('Error cannot make action')
+            
+            except Exception as e:
+                self.get_logger().error(f'Exception during action execution: {str(e)}')
+            
+            time.sleep(1.5)
