4f39a447c5092702eb2cecb5195ef32d665a9d55
kensei-te <81662425+kensei-te@users.noreply.github.com>
2026-01-26 11:27:44 +0900

    Publication code 2026 01 (#8)
    * Update README.md
    
    * bugfix, update
    
    * Remove_noncorresponding_files

--- src/main_process/include/main_process/process_control.hpp
+++ src/main_process/include/main_process/process_control.hpp
@@ -13,22 +13,22 @@ public:
     bool isReadyToNextStep() const;
     bool isSequenceCompleted() const;
     void moveToNextStep();
-    
+
 private:
-    const std::string secquence_file_ = "secquence/seq_kt.txt";
+    const std::string sequence_file_ = "sequence/sequence.txt";
 
     // Device state manager
     DeviceStateManager devices_manager_;
     std::vector<std::string> devices_list_ = {"weighing", "slider", "plc", "cobotta", "record"};
 
-    
+
     // Current step and step index
     std::string current_step_;
     size_t step_index_;
-    
+
     // Process sequences
     std::vector<std::string> sequence_;
-    
+
     void initializeSequences();
 };
 
