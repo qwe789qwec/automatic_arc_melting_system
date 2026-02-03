253eb93d4000fe35a311fd058bebc0deccd47a4d
qwe789qwec <qwe789qwec@gmail.com>
2026-02-03 10:09:28 +0900

    add if and ifnot and rebuild nextStep

--- src/main_process/include/main_process/process_control.hpp
+++ src/main_process/include/main_process/process_control.hpp
@@ -14,8 +14,6 @@ public:
     bool isReadyToNextStep() const;
     bool isSequenceCompleted() const;
     void moveToNextStep();
-    void readSegmentFile(std::string file_name);
-    void handleVariable(const std::string& command);
     
 private:
     const std::string sequence_file_ = "sequence/sequence.txt";
@@ -23,17 +21,23 @@ private:
     // Device state manager
     DeviceStateManager devices_manager_;
     std::vector<std::string> devices_list_ = {"weighing", "slider", "plc", "cobotta", "record"};
-
+    
     // Current step and step index
     std::string current_step_;
     size_t step_index_;
-    std::unordered_map<std::string, size_t> label_map_;
-    std::unordered_map<std::string, size_t> vars_map_;
     
     // Process sequences
     std::vector<std::string> sequence_;
 
+    // Process control
+    std::unordered_map<std::string, size_t> label_map_;
+    std::unordered_map<std::string, size_t> vars_map_;
+    std::vector<std::string> prefixes_ = {"GOTO_", "LABEL_", "VAR_", "IF_", "IFNOT_"};
+    
     void initializeSequences();
+    void readSegmentFile(std::string file_name);
+    void handleVariable(const std::string& command);
+    bool handleConditionals(const std::string& command);
 };
 
 #endif // PROCESS_CONTROL_HPP
